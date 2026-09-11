a6cbbb2698ace89e6aa996df590d710eb6f97d23
777 <pitccr7529@gmail.com>
2026-09-11 16:03:25 +0700

    students

---
+++ Workshop 3/ex1-callback.js
@@ -0,0 +1,6 @@
+const students = [
+  { id: "6501", name: "Somchai", major: "CE", score: 85 },
+  { id: "6502", name: "Somsri", major: "CE", score: 72 },
+  { id: "6503", name: "Anan", major: "LE", score: 64 },
+  { id: "6504", name: "Kanya", major: "IT", score: 90 },
+];
