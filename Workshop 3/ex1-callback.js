282894dee11f070f6af4a01fcfdd4cdba666153c
777 <pitccr7529@gmail.com>
2026-09-11 16:16:11 +0700

    update

--- Workshop 3/ex1-callback.js
+++ Workshop 3/ex1-callback.js
@@ -4,3 +4,28 @@ const students = [
   { id: "6503", name: "Anan", major: "LE", score: 64 },
   { id: "6504", name: "Kanya", major: "IT", score: 90 },
 ];
+
+// ฟังชั่นค้นหานักศึกษาด้วย error-first Callback
+function fetchStudentById(id,Callback){
+    if (typeof id !== "string" || id.trim() ===""){
+        return Callback(new Error("รหัสนักศึกษาไม่ถูกต้อง"))
+
+};
+
+setTimeout(() => {
+    const student = students..find((s) => s.id === id);
+    if(!student){
+        return callbacck(new Error('ไม่พบรหัสนักศึกษา ${id}');)
+    }
+    return callbacck (null,{ ...student});
+  } ,300);
+}
+
+// กรณีไอดีมีจริง
+fetchStudentById("6501",(err,student) => {
+    if(err){
+        console.error("กรณีที่ 1 ผิดพลาด:",err.message);
+        return;
+    }
+    console.log("กรณีที่ 1 พบข้อมูล:",student);
+});
